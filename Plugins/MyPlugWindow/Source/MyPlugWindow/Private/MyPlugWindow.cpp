d40e33e96edd7aa18b289d34e716b0bfdc8fc727
robinwood3d <60308228+robinwood3d@users.noreply.github.com>
2021-04-16 02:38:34 +0800

    提交源工程文件

---
+++ Plugins/MyPlugWindow/Source/MyPlugWindow/Private/MyPlugWindow.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlugWindow.h"
+#include "Widgets/SWindow.h"
+#include "Widgets/SViewport.h"
+#include "Blueprint/UserWidget.h"
+
+// Sets default values
+AMyPlugWindow::AMyPlugWindow()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AMyPlugWindow::SetContent(UUserWidget* Widget)
+{
+	if (Widget)
+	{
+		MyWindow->SetContent(Widget->TakeWidget());
+		MyWidget = Widget;
+	}
+}
+
+// Called when the game starts or when spawned
+void AMyPlugWindow::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MyWindow = SNew(SWindow)
+		.Type(EWindowType::Normal)
+		.Style(&FCoreStyle::Get().GetWidgetStyle<FWindowStyle>("Window"))
+		.Title(Title)
+		.bDragAnywhere(bDragAnywhere)
+		.AutoCenter(static_cast<EAutoCenter>(CenterMode))
+		.ScreenPosition(Position)
+		.ClientSize(Size)
+		.AdjustInitialSizeAndPositionForDPIScale(true)
+		.SupportsTransparency(EWindowTransparency::None)
+		.InitialOpacity(1.f)
+		.IsInitiallyMaximized(false)
+		.IsInitiallyMinimized(false)
+		.SizingRule(static_cast<ESizingRule>(SizingMode))
+		.IsPopupWindow(false)
+		.IsTopmostWindow(true)
+		.FocusWhenFirstShown(true)
+		.ActivationPolicy(EWindowActivationPolicy::Always)
+		.UseOSWindowBorder(bUseOSWindowBorder)
+		.HasCloseButton(bHasCloseButton)
+		.SupportsMaximize(bSupportsMaximize)
+		.SupportsMinimize(bSupportsMinimize)
+		.ShouldPreserveAspectRatio(false)
+		.CreateTitleBar(bCreateTitleBar)
+		.SaneWindowPlacement(true)
+		.LayoutBorder(FMargin(0, 0, 0, 0))
+		.UserResizeBorder(FMargin(5, 5, 5, 5))
+		.bManualManageDPI(false);
+
+	if (MyWindow.IsValid())
+	{
+		if (MyWidget)
+		{
+			MyWindow->SetContent(MyWidget->TakeWidget());
+		}
+		FSlateApplication::Get().AddWindow(MyWindow.ToSharedRef(), true);
+	}
+}
+
+void AMyPlugWindow::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (MyWindow.IsValid())
+	{
+		MyWindow->RequestDestroyWindow();
+		//MyWindow->DestroyWindowImmediately();
+	}
+}
+
+// Called every frame
+void AMyPlugWindow::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Plugins/MyPlugWindow/Source/MyPlugWindow/Public/MyPlugWindow.h
@@ -0,0 +1,100 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyPlugWindow.generated.h"
+
+class SWindow;
+class UUserWidget;
+
+UENUM(BlueprintType)
+enum class ESizingMode : uint8
+{
+	/* The windows size fixed and cannot be resized **/
+	FixedSize,
+
+	/** The window size is computed from its content and cannot be resized by users */
+	Autosized,
+
+	/** The window can be resized by users */
+	UserSized
+};
+
+UENUM(BlueprintType)
+enum class ECenterMode : uint8
+{
+	/** Don't auto-center the window */
+	None,
+
+	/** Auto-center the window on the primary work area */
+	PrimaryWorkArea,
+
+	/** Auto-center the window on the preferred work area, determined using GetPreferredWorkArea() */
+	PreferredWorkArea,
+};
+
+UCLASS()
+class MYPLUGWINDOW_API AMyPlugWindow : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyPlugWindow();
+
+	UFUNCTION(BlueprintCallable, Category = "MyPlugWindow")
+	void SetContent(UUserWidget* Widget);
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	FText Title;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	bool bDragAnywhere = true;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	ECenterMode CenterMode = ECenterMode::None;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	FVector2D Position;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	FVector2D Size;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	ESizingMode SizingMode = ESizingMode::Autosized;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	bool bUseOSWindowBorder = false;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	bool bHasCloseButton = false;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	bool bSupportsMaximize = false;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	bool bSupportsMinimize = false;
+
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	bool bCreateTitleBar = false;
+
+	/** 保存一个窗口内控件的指针*/
+	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
+	UUserWidget* MyWidget;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void EndPlay(const EEndPlayReason::Type EndPlayReason);
+
+	/** 在这个对象里保存这个SWindow对象的指针*/
+	TSharedPtr<SWindow> MyWindow;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
